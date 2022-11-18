9536e7f41baa11d87d4f9ae15d37f7d2ebd5ba42
范龙飞 <634401502@qq.com>
2022-11-18 20:22:40 +0800

    fix：样式

--- components/AreaSwitcher.vue
+++ components/AreaSwitcher.vue
@@ -48,3 +48,9 @@ console.log(baseImgPrefix)
   height: 35px;
 }
 </style>
+<!--<style lang="scss">-->
+<!--:deep(.arco-select-dropdown){-->
+<!--  background: unset;-->
+<!--  box-shadow: unset;-->
+<!--}-->
+<!--</style>-->
