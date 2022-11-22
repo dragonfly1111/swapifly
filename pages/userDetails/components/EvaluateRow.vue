56b3a0e04f81e3bea7647431c850d2a8e6b8a49c
liangjianing <598348433@qq.com>
2022-11-22 23:57:25 +0800

    fix:评价

--- pages/userDetails/components/EvaluateRow.vue
+++ pages/userDetails/components/EvaluateRow.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="evaluate-box">
-    <a-row justify="space-between" align="center" class="box-header">
-      <a-col flex="100px" class="title"> {{ $t("pages.evaluate") }} </a-col>
-      <a-col flex="200px">
-        <a-select v-model="queryForm.s_type">
-          <a-option>Beijing</a-option>
-          <a-option>Shanghai</a-option>
-          <a-option>Guangzhou</a-option>
-        </a-select>
-      </a-col>
-    </a-row>
+    <div class="box-header">
+      <a-row justify="space-between" align="center">
+        <a-col flex="100px" class="title"> {{ $t("pages.evaluate") }} </a-col>
+        <a-col flex="200px">
+          <a-select v-model="queryForm.s_type">
+            <a-option>Beijing</a-option>
+            <a-option>Shanghai</a-option>
+            <a-option>Guangzhou</a-option>
+          </a-select>
+        </a-col>
+      </a-row>
+      <a-row justify="start" align="center" class="evaluate-info">
+        <a-col class="evaluate-total">
+          <div>
+            <span class="total">586</span>
+            <span><icon-star-fill :size="18" /></span>
+          </div>
+          <div>（99{{$t('pages.evaluate')}}）</div>
+        </a-col>
+        <a-col></a-col>
+      </a-row>
+    </div>
+
     <div class="evaluate-box-body"></div>
   </div>
 </template>
@@ -32,6 +45,11 @@ const queryForm = reactive({
   }
   .evaluate-box-body {
     padding: 0 20px 30px;
+    .evaluate-info{
+        .evaluate-total{
+
+        }
+    }
   }
 }
 </style>
