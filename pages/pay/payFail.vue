236fba5496b66fbf7eb93ffe02b013131e5b18ee
范龙飞 <634401502@qq.com>
2022-12-03 11:20:16 +0800

    fix: 支付线上测试

--- pages/pay/payFail.vue
+++ pages/pay/payFail.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="common-row global-content">
-    支付失败页面
+    <div class="pay-res">
+      <img src="@/assets/images/payFail.png" alt="">
+      <div class="title">{{ $t('exposure.payFail') }}</div>
+      <div class="tip">{{ $t('exposure.payFailTip') }}</div>
+      <a-button class="back-but" @click="router.push(`/userDetails?userId=${userInfo.id}`)">{{ $t('exposure.back') }}</a-button>
+    </div>
   </div>
 </template>
 
@@ -10,5 +15,28 @@
 
 <style lang="scss">
 @import "assets/sass/var";
-
+.pay-res{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 250px;
+  .title{
+    color: #333333;
+    margin-top: 23px;
+    font-size: 18px;
+  }
+  .tip{
+    margin-top: 10px;
+    color: $grey-font-label;
+    font-size: 12px;
+  }
+  .back-but{
+    margin-top: 40px;
+    background: $main-pink;
+    color: #FFFFFF;
+    height: 42px;
+    width: 300px;
+  }
+}
 </style>
