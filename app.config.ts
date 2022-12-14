b58eaaa59063e34a1347ba6bd940ee9268d00cac
范龙飞 <634401502@qq.com>
2022-12-14 10:41:33 +0800

    fix: 修复url错误

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-const DOMAIN = 'https://swapiflyapi.honglanshuzi.com/swapifly'
+const DOMAIN = 'https://swapiflyapi.honglanshuzi.com'
 
 export default defineAppConfig({
   name: 'Swapifly - 24小時無休二手買賣平台。集結全球買家、賣家，輕鬆轉讓二手物品， 不論你喺香港定已經移民海外，上Swapifly 一定幫到你。早鳥優惠，注冊賣家帳戶，$0開網店! ',
