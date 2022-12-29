63200303ede8f419b122a7a2dd705a3aced84e79
范龙飞 <634401502@qq.com>
2022-12-29 21:05:41 +0800

    fix: 移除ele

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -1,13 +1,13 @@
-/*
- * @Description: 
- * @Autor: ljn
- * @Date: 2022-12-29 17:52:25
- * @LastEditors: ljn
- * @LastEditTime: 2022-12-29 17:52:37
- */
-import { defineNuxtPlugin } from '#app'
-import  'element-plus/dist/index.full'
-import ElementPlus from 'element-plus'
-export default defineNuxtPlugin(nuxtApp=>{
-    nuxtApp.vueApp.use(ElementPlus)
-})
+// /*
+//  * @Description:
+//  * @Autor: ljn
+//  * @Date: 2022-12-29 17:52:25
+//  * @LastEditors: ljn
+//  * @LastEditTime: 2022-12-29 17:52:37
+//  */
+// import { defineNuxtPlugin } from '#app'
+// import  'element-plus/dist/index.full'
+// import ElementPlus from 'element-plus'
+// export default defineNuxtPlugin(nuxtApp=>{
+//     nuxtApp.vueApp.use(ElementPlus)
+// })
