c11f608c1a307fcd826b444850a36daa0b5c9261
liangjianing <liangjianing@qq.com>
2022-12-30 17:52:56 +0800

    fix:facebook按钮修改

--- components/ThirdLoginWrap.vue
+++ components/ThirdLoginWrap.vue
@@ -114,6 +114,7 @@ const loginThird = (type: number) =>{
     justify-content: center;
     width: 100%;
     padding: 7px 0;
+    cursor: pointer;
     img{
       width: 35px;
       object-fit: contain;
