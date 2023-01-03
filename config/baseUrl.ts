238ff71ebb8d879056e92e2f75a0fa94d62bb9a2
范龙飞 <634401502@qq.com>
2023-01-03 16:54:58 +0800

    fix: 分享链接修改

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseApiPrefix = 'https://swapiflyapi.honglanshuzi.com/index.php' // 测试环境
-// export const baseApiPrefix = 'https://swapifly.com/index.php' // 正式环境
+// export const baseApiPrefix = 'https://swapiflyapi.honglanshuzi.com/index.php' // 测试环境
+export const baseApiPrefix = 'https://swapifly.com/index.php' // 正式环境
