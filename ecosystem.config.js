ad05af8c2f280e15162df5f9d7c72101bbaaa34d
范龙飞 <634401502@qq.com>
2023-01-03 11:28:49 +0800

    fix: 修改pm2启动端口配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,15 @@ module.exports = {
       name: 'swapifly',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs',
+      env_production: {
+        PORT: 3000,
+        NODE_ENV: "production"
+      },
+      env_test: {
+        PORT: 3001,
+        NODE_ENV: "development"
+      }
     }
   ]
 }
