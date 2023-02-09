ba0700c137538d134df293f1bca5c3af517b50c4
范龙飞 <634401502@qq.com>
2023-02-09 12:00:43 +0800

    fix: 广告暂时移除骨架屏

--- components/AD.vue
+++ components/AD.vue
@@ -2,14 +2,14 @@
   <div class="ad-wrapper" :style="{ width: width }">
     <div class="ad-title" v-if="props.hasTitle">{{ $t("foot.AD") }}</div>
     <div v-if="advert" class="ad-content" :style="{ height: height }" v-html="advert"></div>
-    <div v-else class="ad-content">
-      <div>
-        <a-skeleton :animation="true" :loading="true">
-          <div style="width: 100%">
-            <a-skeleton-line :line-height="parseInt(height)" />
-          </div>
-        </a-skeleton>
-      </div>
+    <div v-else class="ad-content" :style="{ height: height }">
+<!--      <div>-->
+<!--        <a-skeleton :animation="true" :loading="true">-->
+<!--          <div style="width: 100%">-->
+<!--            <a-skeleton-line :line-height="parseInt(height)" />-->
+<!--          </div>-->
+<!--        </a-skeleton>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
